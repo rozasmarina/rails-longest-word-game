3bfd1e5b3fee25413c0f932c065e1d3a6f79b973
Marina Rozas <rozasmarina@gmail.com>
2020-08-17 17:33:44 -0300

    scoring done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,20 +7,26 @@ class GamesController < ApplicationController
   end
 
   def score
+    start_time = DateTime.parse(params[:time])
+    end_time = Time.now
+    total_time = end_time - start_time
     @word = params[:input]
-
+    
+    
     url= open("https://wagon-dictionary.herokuapp.com/#{@word}").read
     obj = JSON.parse(url)
     
     if check_attempt(@word)
       if obj["found"]
-        @message = "Well done!"
+        total_score = (@word.length * 100) - total_time
+        @result = {message: "Well done!", time: total_time, score: total_score }
       else
-        @message = "#{@word} is not an english word"
+        @result = {message: "#{@word} is not an english word", time: total_time, score: 0 }
       end
     else
-      @message = "#{@word} is not on the grid"
+      @result = {message: "#{@word} is not on the grid", time: total_time, score: 0 }
     end
+    @result
   end
 
   def check_attempt (word)
